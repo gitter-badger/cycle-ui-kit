85d0630e005c67a57be837597cdb0f7cdf873220
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-19 06:35:49 +0530

    Adding the interfaces

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,14 +1,21 @@
 import { DOMSource } from '@cycle/dom/xstream-typings';
 import xs, { Stream } from 'xstream';
+import { VNode } from '@cycle/dom';
 
 export interface IButtonSources {
-
+  dom: DOMSource;
+  disabled$: Stream<boolean>;
 }
 
 export interface IButtonSinks {
-  
+  dom: Stream<VNode>;
+  click$: Stream<MouseEvent>;
+  hover$: Stream<MouseEvent>;
+  focus$: Stream<FocusEvent>;
+  pressed$: Stream<boolean>;
+  disabled$: Stream<boolean>;
 }
 
 export function Button(sources: IButtonSources): IButtonSinks {
-  return {};
+  return null;
 }
