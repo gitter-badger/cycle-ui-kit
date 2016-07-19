ef0f5f0238775ed21b11b334e12c5b25569cca06
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-19 06:32:40 +0530

    Starting to write a Button component

---
+++ src/components/button/index.ts
@@ -0,0 +1,14 @@
+import { DOMSource } from '@cycle/dom/xstream-typings';
+import xs, { Stream } from 'xstream';
+
+export interface IButtonSources {
+
+}
+
+export interface IButtonSinks {
+  
+}
+
+export function Button(sources: IButtonSources): IButtonSinks {
+  return {};
+}
